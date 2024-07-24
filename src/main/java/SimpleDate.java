3a1b37852a42bc45b1f3dfc2644fdb97ee9a32b8
Daniil Starov <jazzzydan@hotmail.com>
2024-07-24 12:35:19 +0300

    Initial commit

---
+++ src/main/java/SimpleDate.java
@@ -0,0 +1,74 @@
+
+public class SimpleDate {
+
+    private int day;
+    private int month;
+    private int year;
+
+    public SimpleDate(int day, int month, int year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return this.day + "." + this.month + "." + this.year;
+    }
+
+    public boolean earlier(SimpleDate other) {
+        if (this.year < other.year) {
+            return true;
+        }
+
+        if (this.year == other.year
+                && this.month < other.month) {
+            return true;
+        }
+
+        if (this.year == other.year
+                && this.month == other.month
+                && this.day < other.day) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public int differenceInYears(SimpleDate other) {
+        if (earlier(other)) {
+            return other.differenceInYears(this);
+        }
+
+        int yearRemoved = 0;
+
+        if (this.month < other.month) {
+            yearRemoved = 1;
+        } else if (this.month == other.month && this.day < other.day) {
+            yearRemoved = 1;
+        }
+
+        return this.year - other.year - yearRemoved;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        SimpleDate that = (SimpleDate) obj;
+        return day == that.day &&
+                month == that.month &&
+                year == that.year;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+        // Choose prime numbers to reduce collisions
+        result = 31 * result + day;
+        result = 31 * result + month;
+        // Weights based on the year range
+        result = 31 * result + (year - 1900); // Normalize year to start from 0
+        return result;
+    }
+}
